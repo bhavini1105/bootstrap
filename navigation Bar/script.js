00c3a9e7446ccce5cb2e6bae5d69aad2f4d1f1df
bhavini1105 <bhavinipatel7532@gmail.com>
2024-05-11 12:05:29 +0530

    Bootsrap navigation

---
+++ navigation Bar/script.js
@@ -0,0 +1,12 @@
+// $("#module").hide();
+
+// $("#navbaricon").on("click",function(){
+//     console.log("hello");
+//       $("#module").toggle()
+//       $("#nav").hide()
+//   })
+
+//   $("#close").on("click",function(){
+//     $("#module").hide()
+//     $("#nav").show()
+//   })
